d85da236e5b6f6aeadb998587928140c4b531972
Jared Bates <computerwizjared@hotmail.com>
2014-11-08 09:42:31 -0600

    Added SpigotAPI to POM and base code structure sorta

---
+++ src/main/java/me/Un1ted/Pancakes/Main.java
@@ -0,0 +1,13 @@
+package me.Un1ted.Pancakes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
