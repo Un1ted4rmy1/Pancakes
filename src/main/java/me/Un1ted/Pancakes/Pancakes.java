0f7ec766eee4adba25904d7c0a8feab2d051dc0b
IC-Un1ted <ryangoldberg@rocketmail.com>
2014-11-09 01:27:04 -0500

    Added a mandatory mysql connection when the plugin is enabled.

--- src/main/java/me/Un1ted/Pancakes/Pancakes.java
+++ src/main/java/me/Un1ted/Pancakes/Pancakes.java
@@ -1,18 +1,41 @@
 package me.Un1ted.Pancakes;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+
+import me.Un1ted.SQL.SQLAPI;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Pancakes extends JavaPlugin {
 	private static Pancakes instance;
+	public static Connection sqlConnection = null;
 	
 	public void onEnable() {
 		instance = this;
+		//Starts the MYSQL Connection - Required
+		startConnection();
 	}
 	
 	public void onDisable() {
 		
 	}
 	
+	public void startConnection()
+	{
+		try 
+		{
+			sqlConnection = SQLAPI.getConnection();
+		}
+		catch (SQLException e)
+		{
+			getLogger().log(Level.SEVERE, "Unable to find database. Disabling plugin.");
+			Bukkit.broadcastMessage("Unable to connect to the database. Core Management plugin disabled.");
+			this.setEnabled(false);
+		}
+	}
 	public static Pancakes getPlugin() {
 		return instance;
 	}
